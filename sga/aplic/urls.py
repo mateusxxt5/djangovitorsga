f1e0f4f9e31b3c3fade018082b16721ca491f03c
Pedro Ferreira <pedro.p.ferreira@hotmail.com>
2024-11-27 14:35:20 -0300

    adicionada página de login/cadastro e formulario e estilização feita

--- sga/aplic/urls.py
+++ sga/aplic/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import IndexView
-
+from . import views
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('cadastro/', views.CadastroView.as_view(), name='cadastro'),
+    path('', views.IndexView.as_view(), name='index'),  # Página principal
+    path('produtos/', views.ProdutosView.as_view(), name='produtos'),  # Página de Produtos
 ]
--- sga/aplic/views.py
+++ sga/aplic/views.py
@@ -1,8 +1,12 @@
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
 from .models import Produto
 
-# Create your views here.
+# Página principal
 class IndexView(TemplateView):
     template_name = 'index.html'
 
@@ -10,3 +14,28 @@ class IndexView(TemplateView):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['Produtos'] = Produto.objects.order_by('-nome').all()
         return context
+
+# Página de Produtos
+class ProdutosView(TemplateView):
+    template_name = 'produtos.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['Produtos'] = Produto.objects.all()
+        return context
+
+# Login
+class CustomLoginView(LoginView):
+    template_name = 'login_cadastro.html'  # Template para a página de login
+    redirect_authenticated_user = True  # Redireciona usuários logados para a home
+
+# Cadastro
+class CadastroView(FormView):
+    template_name = 'login_cadastro.html'  # Mesmo template para login e cadastro
+    form_class = UserCreationForm
+    success_url = '/'  # Redirecionar após o cadastro
+
+    def form_valid(self, form):
+        user = form.save()  # Salva o novo usuário
+        login(self.request, user)  # Loga o novo usuário automaticamente
+        return redirect(self.success_url)  # Redireciona para a página principal
